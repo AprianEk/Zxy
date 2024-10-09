afea15ad05bae2ac67444235d81dd3df2fba20ba
Xyx <aprianek@gmail.com>
2024-10-09 14:51:19 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Computer Science Student", "Frontend Developer Beginner &#128511;", "Web Developer Beginner &#128511;"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
